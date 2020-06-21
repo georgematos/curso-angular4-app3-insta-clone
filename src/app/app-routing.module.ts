d860b9253fa62dfcb112de7cea2f6ffe859476bb
georgematos <georgemattos@gmail.com>
2020-06-21 17:50:20 -0300

    codigo independente - criando rotas para cadastro e login na tela de acesso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CadastroComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
