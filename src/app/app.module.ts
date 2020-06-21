d6360a104ad22d7ddc2fd2511bac6e999530c2c1
georgematos <georgemattos@gmail.com>
2020-06-21 18:56:53 -0300

    configurando o angular para suportar web-animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BannerComponent } from './acesso/banner/banner.component';
-import { LoginComponent } from './acesso/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import { LoginComponent } from './acesso/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-
     AcessoComponent,
+    AppComponent,
     BannerComponent,
+    CadastroComponent,
     LoginComponent,
-    CadastroComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
