6e98377422d6a1d907f4a57b93aca05e5067dd16
georgematos <georgemattos@gmail.com>
2020-07-14 16:49:49 -0300

    cadastrando usuarios no firebase - parte 2

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,7 +4,15 @@ import * as firebase from 'firebase';
 export class Auth {
     public cadastrar(usuario: Usuario): void {
         firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
-            .then((resp): any => console.log(resp))
+            .then((resp): any => {
+                // remover atributo senha do usuario
+                delete usuario.senha
+
+                // registrando dados complementares do usuario no path email na base 64
+                // btoa: funcao nativa js que converte uma string para base 64 atob faz o contrário
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`) 
+                    .set(usuario);
+            })
             .catch((error: Error) => console.log(error));
     }
 }
