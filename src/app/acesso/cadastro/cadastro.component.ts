7e1354074f0c693f57a1b90dd5dfcaea1bf1c0fc
georgematos <georgemattos@gmail.com>
2020-07-14 17:59:18 -0300

    controlando fomulario de login

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -10,41 +10,7 @@ import { Auth } from '../../auth.service';
 })
 export class CadastroComponent implements OnInit {
 
-    public formulario: FormGroup = new FormGroup({
-      'email': new FormControl(null),
-      'nome_completo': new FormControl(null),
-      'nome_usuario': new FormControl(null),
-      'senha': new FormControl(null)
-    })
-    
-    @Output()
-    public exibirPainel: EventEmitter<string> = new EventEmitter<string>();
-    
-    constructor(
-      private fb: FormBuilder,
-      public auth: Auth
-  ) { }
-
-  ngOnInit(): void {
-    this.formValidationInit();
-  }
-
-  public exibirPainelDeLogin(): void {
-    this.exibirPainel.emit('login');
-  }
-
-  public cadastrarUsuario(): void {
-    let usuario: Usuario = new Usuario(
-      this.formulario.value.nome_usuario,
-      this.formulario.value.nome_completo,
-      this.formulario.value.email,
-      this.formulario.value.senha
-    );
-    this.auth.cadastrar(usuario);
-  }
-
-  private formValidationInit(): void {
-    this.formulario = this.fb.group({
+    public formulario = this.fb.group({
       nome_usuario: ['', Validators.compose([
         Validators.required,
         Validators.minLength(3),
@@ -63,5 +29,30 @@ export class CadastroComponent implements OnInit {
         Validators.required,
       ])],
     })
+    
+    @Output()
+    public exibirPainel: EventEmitter<string> = new EventEmitter<string>();
+    
+    constructor(
+      private fb: FormBuilder,
+      public auth: Auth
+  ) { }
+
+  ngOnInit(): void {
   }
+
+  public exibirPainelDeLogin(): void {
+    this.exibirPainel.emit('login');
+  }
+
+  public cadastrarUsuario(): void {
+    let usuario: Usuario = new Usuario(
+      this.formulario.value.nome_usuario,
+      this.formulario.value.nome_completo,
+      this.formulario.value.email,
+      this.formulario.value.senha
+    );
+    this.auth.cadastrar(usuario);
+  }
+
 }
--- src/app/acesso/login/login.component.ts
+++ src/app/acesso/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +8,33 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public formulario = this.fb.group({
+    'email': ['', Validators.compose([
+      Validators.required,
+      Validators.email,
+    ])],
+    'senha': ['', Validators.compose([
+      Validators.required,
+      Validators.minLength(6)
+    ])]
+  })
+
   @Output()
   public exibirPainel: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
   public exibirPainelDeLogin(): void {
     this.exibirPainel.emit('cadastro');
+  }
+
+  public autenticar(): void {
+    console.log(this.formulario.value.email,"\n",this.formulario.value.senha);
   }
 
 }
