8a96fb60c96d17314912e2ec28142e678ab5cc36
georgematos <georgemattos@gmail.com>
2020-07-22 16:54:06 -0300

    ajustando formulario modal de publicacao com reactive forms e formBuilder

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-incluir-publicacao',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncluirPublicacaoComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup = this.fb.group({
+    'titulo': [''],
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+  }
+
+  public publicar(): void {
+    console.log(this.formulario.value.titulo);
   }
 
 }
